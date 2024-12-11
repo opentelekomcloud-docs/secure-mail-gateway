8362853aa6c4cd32b0db9c7b0ac7508e9382c9ae
SebastianGode <70581801+SebastianGode@users.noreply.github.com>
2024-12-11 14:35:58 +0100

    Update tox.ini && conf.py file (#24)
    Update tox.ini && conf.py file
    
    Performed-by: gitea/infra/otc-metadata/tools/generate_doc_confpy.py
    
    Reviewed-by: Tino Schr

--- umn/source/conf.py
+++ umn/source/conf.py
@@ -110,7 +110,7 @@ html_copy_source = False
 latex_documents = [
     ('index',
      'smg-umn.tex',
-     u'Secure Mail Gateway - User Guide',
+    u'Secure Mail Gateway - User Guide',
      u'OpenTelekomCloud', 'manual'),
 ]
 
